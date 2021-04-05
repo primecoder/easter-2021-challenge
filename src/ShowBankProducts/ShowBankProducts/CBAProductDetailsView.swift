a28ca2b5551621565add5941dd68ce26c96a46c2
Ace AUTHORS <ace@T-800.local>
2021-04-05 14:16:01 +1000

    UI work on product details page

--- src/ShowBankProducts/ShowBankProducts/CBAProductDetailsView.swift
+++ src/ShowBankProducts/ShowBankProducts/CBAProductDetailsView.swift
@@ -17,17 +17,28 @@ struct CBAProductDetailsView: View {
     }
 
     var body: some View {
-        Text(cbaVm.productDetails?.name ?? "...")
-            .bold()
-            .font(.title)
-            .lineLimit(3)
-        
-        ScrollView {
-            if let prodDetails = cbaVm.productDetails {
-                ProductDetailsView(productDetails: prodDetails)
-                    .padding()
+        Group  {
+            Text(cbaVm.productDetails?.name ?? "...")
+                .bold()
+                .font(.title)
+                .lineLimit(3)
+                .background(Color(red: 0.996, green: 0.734, blue: 0.058))
+            
+            ScrollView {
+                if let prodDetails = cbaVm.productDetails {
+                    ProductDetailsView(productDetails: prodDetails)
+                        .padding()
+                }
             }
+            .background(Color.white)
         }
+        .padding(.top, 32)
+        .background(Color(red: 0.996, green: 0.734, blue: 0.058))
+        .cornerRadius(12)
+        .padding()
+        .shadow(color: .gray, radius: 5, x: 0.0, y: 0.0)
+        .background(Color(red:0.803, green:0.779, blue:0.779))
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
@@ -48,7 +59,7 @@ struct ProductDetailsView: View {
             
             Picker("", selection: $selectedSection) {
                 Text("Eligibility").tag(0)
-                Text("Fatures").tag(1)
+                Text("Features").tag(1)
                 Text("Fees").tag(2)
                 Text("Rates").tag(3)
             }
@@ -72,6 +83,27 @@ struct ProductDetailsView: View {
                         ProductDetailsFeatureView(productFeature: feature)
                     }
                 }
+            case 2:
+                VStack(alignment: .leading) {
+                    Text("Fees:")
+                        .bold()
+                    if let fees = productDetails.fees {
+                        ForEach(Array(fees.enumerated()), id: \.offset) { idx, fee in
+                            ProductFeeView(fee: fee)
+                        }
+                    }
+                }
+            case 3:
+                VStack(alignment: .leading) {
+                    Text("Deposit Rates:")
+                        .bold()
+                    if let rates = productDetails.depositRates {
+                        ForEach(Array(rates.enumerated()), id: \.offset) { idx, rate in
+                            ProductRateView(rate: rate)
+                                .padding(.horizontal)
+                        }
+                    }
+                }
             default:
                 Text("Unsupported selection")
             }
@@ -82,9 +114,52 @@ struct ProductDetailsView: View {
     }
 }
 
+struct ProductRateView: View {
+    var rate: CommbankProductDetails.DepositRate
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading) {
+                Text(rate.depositRateType ?? "?")
+                VStack(alignment: .trailing) {
+                    HStack { Text("Calculate freq:"); Text(rate.calculationFrequency ?? "-") }
+                    HStack { Text("Application freq:"); Text(rate.applicationFrequency ?? "-") }
+                    HStack { Text("Additional value:"); Text(rate.additionalValue ?? "-") }
+                }
+                .font(.system(size: 14))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 32)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+
+            Text(rate.rate ?? "--.-")
+
+        }
+        .frame(maxWidth: .infinity, alignment: .top)
+        .padding(.bottom)
+    }
+}
+
+struct ProductFeeView: View {
+    var fee: CommbankProductDetails.Fee
+    var body: some View {
+        if let amount = fee.amount {
+            HStack {
+                Text(fee.name ?? "(n/a)")
+                    .lineLimit(2)
+                Spacer()
+                HStack {
+                    Text("$\(amount)")
+                    Text("AUD").font(.footnote)
+                }
+            }
+            .padding(.leading)
+            .frame(height: 50)
+        }
+    }
+}
+
 struct ProductEligibilityView: View {
     var eligibility: CommbankProductDetails.Eligibility
-    
     var body: some View {
         VStack(alignment: .leading) {
             HStack {
@@ -111,7 +186,6 @@ struct ProductEligibilityView: View {
 
 struct ProductDetailsFeatureView: View {
     var productFeature: CommbankProductDetails.Feature
-    
     var body: some View {
         HStack(alignment: .top) {
             Image(systemName: "checkmark.circle")
